282a5cb267f266bc51806661528e05e224ec635c
Phillip Lu <Plu2978@conestogac.on.ca>
2024-02-28 11:16:16 -0500

    completed profile and edit page

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,6 @@ import { derived, writable, type Readable } from "svelte/store";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDw9JLeLulv4NWcfXmiASirOhw8OCY0fCQ",
   authDomain: "svelteapp-ae112.firebaseapp.com",
@@ -16,7 +15,6 @@ const firebaseConfig = {
   storageBucket: "svelteapp-ae112.appspot.com",
   messagingSenderId: "57862613065",
   appId: "1:57862613065:web:82f060baffc23e522cf798",
-  measurementId: "G-G4HER55F96"
 };
 
 // Initialize Firebase
