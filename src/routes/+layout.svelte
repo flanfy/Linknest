4520f92a22ad871c693fc7ec9dc1383f35eccab9
Phillip Lu <Plu2978@conestogac.on.ca>
2024-03-02 16:55:55 -0500

    added settings dropdown

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,16 @@
 <script lang="ts">
-    import { auth, user, userData } from "$lib/firebase";
-  import { signOut } from "firebase/auth";
-     import "../app.css";
+  import { user, userData } from "$lib/firebase";
+  import "../app.css";
+  import Dropdown from "$lib/components/Dropdown.svelte";
     $user;
     $userData; 
-
-    async function signOutSSR() {
-    const res = await fetch("/api/signin", { method: "DELETE" });
-    await signOut(auth);
-    window.location.href = "/";
-  }
  </script>
  
  <div class="min-h-screen flex flex-col">
     <div class="flex justify-between items-center px-4 py-2">
       <a href="/" class="text-xl font-bold text-purple-400 hover:text-purple-500">Linknest</a>
         {#if $userData?.username}
-            <div class="flex">
-            <a href="/{$userData?.username}" class="ml-4 text-lg text-purple-400 hover:text-purple-500">Your Profile</a>
-            <a href="/{$userData?.username}/edit" class="ml-4 text-lg text-purple-400 hover:text-purple-500">Edit Profile</a>
-            <a href="/" class="ml-4 text-lg text-purple-400 hover:text-purple-500" on:click|preventDefault={signOutSSR}>Sign Out</a>
-            </div>
+          <Dropdown></Dropdown>
         {/if}
     </div>
     <slot />
